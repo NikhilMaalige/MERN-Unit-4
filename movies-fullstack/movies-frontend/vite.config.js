7e3a3011afff1ca33f00220c0900b12b54001ddd
NikhilMaalige <nmaalige@gmail.com>
2024-11-19 15:44:37 -0500

    proxy revised

--- movies-fullstack/movies-frontend/vite.config.js
+++ movies-fullstack/movies-frontend/vite.config.js
@@ -1,13 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
+  server: {
     proxy: {
-      "/api": "http://localhost:3001",
-      changeOrigin: true,
-    }
-  }
-})
+      "/api": { target: "http://localhost:3001", changeOrigin: true },
+    },
+  },
+});
